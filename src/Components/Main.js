ad0d8ac39214463fba69fc83c1a5df74d04c1b90
prem <premkumar.k4898@gmail.com>
2022-03-07 22:27:56 +0530

    First commit

---
+++ src/Components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Title from './Title';
+
+
+class Main extends Component {
+    render() {
+        return (<div>
+            <Title title={'Photowall'} />
+        </div>)
+
+    }
+}
+
+export default Main;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Main from './Components/Main';
+import './styles/stylesheet.css'
+
+
+
+ReactDom.render(<Main />, document.getElementById('root'));
